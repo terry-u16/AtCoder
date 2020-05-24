63528d2e611d0a811f6287da33535277ec0f8493
terry_u16 <terry-u16@users.noreply.github.com>
2020-05-24 11:37:33 +0900

    Add TestCaseGenerator

---
+++ TestCaseGenerator/TestCaseGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCaseGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string inputFilePath = "input.txt";
+            const string outputFilePath = "output.txt";
+
+            WriteInput(new StreamWriter(inputFilePath, false, Encoding.UTF8));
+            WriteOutput(new StreamWriter(outputFilePath, false, Encoding.UTF8));
+        }
+
+        static void WriteInput(TextWriter writer)
+        {
+            writer.WriteLine(500);
+            writer.WriteLine(string.Join(" ", Enumerable.Range(1, 500 * 500)));
+        }
+
+        static void WriteOutput(TextWriter writer)
+        {
+
+        }
+    }
+}

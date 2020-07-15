98ae6492a71480bf651e502d95b4e424a0a2c22d
terry_u16 <terry-u16@users.noreply.github.com>
2020-07-16 00:07:10 +0900

    Add Training20200715

---
+++ Training20200715/Training20200715/Training20200715/Questions/QuestionA.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Training20200715.Algorithms;
+using Training20200715.Collections;
+using Training20200715.Extensions;
+using Training20200715.Numerics;
+using Training20200715.Questions;
+
+namespace Training20200715.Questions
+{
+    /// <summary>
+    /// https://atcoder.jp/contests/abc114/tasks/abc114_b
+    /// </summary>
+    public class QuestionA : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            var s = inputStream.ReadLine();
+
+            var min = int.MaxValue;
+            for (int i = 0; i + 2 < s.Length; i++)
+            {
+                min = Math.Min(min, GetAbs(s.AsSpan(i, 3)));
+            }
+
+            yield return min;
+        }
+
+        int GetAbs(ReadOnlySpan<char> s)
+        {
+            var n = int.Parse(s);
+            return Math.Abs(n - 753);
+        }
+    }
+}
---
+++ Training20200715/Training20200715/Training20200715/Questions/QuestionE.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Training20200715.Algorithms;
+using Training20200715.Collections;
+using Training20200715.Extensions;
+using Training20200715.Numerics;
+using Training20200715.Questions;
+
+namespace Training20200715.Questions
+{
+    public class QuestionE : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

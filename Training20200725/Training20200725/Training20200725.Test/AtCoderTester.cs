739b22def6f82dac6592073bcb533834065f9cb0
terry_u16 <terry-u16@users.noreply.github.com>
2020-07-26 00:22:43 +0900

    Add Training20200725

---
+++ Training20200725/Training20200725/Training20200725.Test/AtCoderTester.cs
@@ -0,0 +1,118 @@
+using System;
+using Xunit;
+using Training20200725.Questions;
+using System.Collections.Generic;
+using System.Linq;
+using Training20200725.Collections;
+
+namespace Training20200725.Test
+{
+    public class AtCoderTester
+    {
+        [Theory]
+        [InlineData(@"10
+7 6
+3 2
+2 4
+4 5
+8 9
+1 8
+1 6
+1 2
+9 10", @"5 10")]
+//        [InlineData(@"4
+//1 2
+//1 3
+//1 4", @"4 3")]
+        public void QuestionATest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionA();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        [Theory]
+        [InlineData(@"4 5
+100 4 60 1 4", @"160")]
+        [InlineData(@"10 1
+5000 2 2000 1 300", @"20000")]
+        [InlineData(@"9 23
+170 8 120 5 12", @"650")]
+        [InlineData(@"653 314159
+6728 123456 5141 41928 222222", @"2818162")]
+        public void QuestionBTest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionB();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        [Theory]
+        [InlineData(@"4 10", @"6")]
+        [InlineData(@"100 450", @"538992043")]
+        [InlineData(@"5 2", @"15")]
+        public void QuestionCTest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionC();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        //[Theory]
+        //[InlineData(@"", @"")]
+        public void QuestionDTest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionD();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        //[Theory]
+        //[InlineData(@"", @"")]
+        public void QuestionETest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionE();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        //[Theory]
+        //[InlineData(@"", @"")]
+        public void QuestionFTest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionF();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        void AssertNearlyEqual(IEnumerable<string> expected, IEnumerable<string> actual, double acceptableError = 1e-6)
+        {
+            Assert.Equal(expected.Count(), actual.Count());
+            foreach (var (exp, act) in (expected, actual).Zip().Select(p => (double.Parse(p.v1), double.Parse(p.v2))))
+            {
+                var error = act - exp;
+                Assert.InRange(Math.Abs(error), 0, acceptableError);
+            }
+        }
+
+        IEnumerable<string> SplitByNewLine(string input) => input?.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None) ?? new string[0];
+    }
+}
---
+++ Training20200725/Training20200725/Training20200725/Questions/QuestionB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Training20200725.Algorithms;
+using Training20200725.Collections;
+using Training20200725.Extensions;
+using Training20200725.Numerics;
+using Training20200725.Questions;
+
+namespace Training20200725.Questions
+{
+    /// <summary>
+    /// https://atcoder.jp/contests/abc013/tasks/abc013_3
+    /// </summary>
+    public class QuestionB : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            var (totalDays, initialManpuku) = inputStream.ReadValue<int, int>();
+            var (normalCost, normalManpuku, shissoCost, shissoManpuku, hungry) = inputStream.ReadValue<int, int, int, int, int>();
+
+            normalManpuku += hungry;
+            shissoManpuku += hungry;
+            long needed = (long)hungry * totalDays - initialManpuku + 1;
+
+            long minCost = long.MaxValue;
+            for (long normalMeals = 0; normalMeals <= totalDays; normalMeals++)
+            {
+                var last = needed - normalManpuku * normalMeals;
+                var shissoMeals = Math.Max((last + shissoManpuku - 1) / shissoManpuku, 0);
+
+                if (normalMeals + shissoMeals <= totalDays)
+                {
+                    minCost = Math.Min(minCost, normalCost * normalMeals + shissoCost * shissoMeals);
+                }
+            }
+
+            yield return minCost;
+
+        }
+    }
+}

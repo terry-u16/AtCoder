6d369eeb951380b81c00feb2c903c6b600397869
terry_u16 <terry-u16@users.noreply.github.com>
2020-07-29 22:34:46 +0900

    Add review of Kujikatsu045

--- Kujikatsu045/Kujikatsu045/Kujikatsu045/Questions/QuestionF.cs
+++ Kujikatsu045/Kujikatsu045/Kujikatsu045/Questions/QuestionF.cs
@@ -20,96 +20,38 @@ namespace Kujikatsu045.Questions
     public class QuestionF : AtCoderQuestionBase
     {
         BasicGraph graph;
-        bool[] seen;
+        Color[] colors;
 
         public override IEnumerable<object> Solve(TextReader inputStream)
         {
             var (nodeCount, edgeCount) = inputStream.ReadValue<int, int>();
-            graph = new BasicGraph(nodeCount * 2);
-            var edges = new BasicEdge[edgeCount * 4];
-            seen = new bool[nodeCount * 2];
+            graph = new BasicGraph(nodeCount);
+            colors = new Color[nodeCount];
 
             for (int i = 0; i < edgeCount; i++)
             {
                 var (a, b) = inputStream.ReadValue<int, int>();
                 a--;
                 b--;
-
-                if (a > b)
-                {
-                    (a, b) = (b, a);
-                }
-
-                edges[i * 4] = new BasicEdge(a, b + nodeCount);
-                edges[i * 4 + 1] = new BasicEdge(b + nodeCount, a);
-                edges[i * 4 + 2] = new BasicEdge(a + nodeCount, b);
-                edges[i * 4 + 3] = new BasicEdge(b, a + nodeCount);
-                for (int j = 0; j < 4; j++)
-                {
-                    graph.AddEdge(edges[i * 4 + j]);
-                }
+                graph.AddEdge(new BasicEdge(a, b));
+                graph.AddEdge(new BasicEdge(b, a));
             }
 
-            Bfs();
-
-            if (seen.All(b => b))
+            if (Paint())
+            {
+                yield return (long)colors.Count(c => c == Color.Black) * colors.Count(c => c == Color.White) - edgeCount;
+            }
+            else
             {
                 yield return (long)nodeCount * (nodeCount - 1) / 2 - edgeCount;
-                yield break;
             }
-
-            long result = 0;
-            var odds = new int[nodeCount + 1];
-            var evens = new int[nodeCount + 1];
-
-            for (int i = 0; i < nodeCount; i++)
-            {
-                evens[i + 1] = evens[i] + (seen[i] ? 1 : 0);
-                odds[i + 1] = odds[i] + (seen[nodeCount + i] ? 1 : 0);
-            }
-
-            for (int i = nodeCount - 1; i >= 0; i--)
-            {
-                // 偶数
-                if (seen[i])
-                {
-                    result += odds[i];
-                }
-
-                // 奇数
-                if (seen[i + nodeCount])
-                {
-                    result += evens[i];
-                }
-            }
-
-            for (int i = 0; i < edgeCount; i++)
-            {
-                var odd = edges[i * 4].From.Index;
-                var even = edges[i * 4].To.Index;
-
-                if (seen[odd] && seen[even])
-                {
-                    result--;
-                }
-
-                odd = edges[i * 4 + 2].To.Index;
-                even = edges[i * 4 + 2].From.Index;
-
-                if (seen[odd] && seen[even])
-                {
-                    result--;
-                }
-            }
-
-            yield return result;
         }
 
-        void Bfs()
+        bool Paint()
         {
             var todo = new Queue<int>();
             todo.Enqueue(0);
-            seen[0] = true;
+            colors[0] = Color.Black;
 
             while (todo.Count > 0)
             {
@@ -117,13 +59,27 @@ namespace Kujikatsu045.Questions
                 foreach (var edge in graph[current])
                 {
                     var next = edge.To.Index;
-                    if (!seen[next])
+
+                    if (colors[next] == Color.None)
                     {
                         todo.Enqueue(next);
-                        seen[next] = true;
+                        colors[next] = colors[current] == Color.Black ? Color.White : Color.Black;
+                    }
+                    else if (colors[current] == colors[next])
+                    {
+                        return false;
                     }
                 }
             }
+
+            return true;
+        }
+
+        enum Color
+        {
+            None,
+            Black,
+            White
         }
     }
 }

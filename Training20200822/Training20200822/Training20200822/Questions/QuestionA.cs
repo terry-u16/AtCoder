206163de171c0bf105c2ea8506c85bbd96add0ec
terry_u16 <terry-u16@users.noreply.github.com>
2020-08-23 00:48:13 +0900

    Add Training20200822

---
+++ Training20200822/Training20200822/Training20200822/Questions/QuestionA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Training20200822.Algorithms;
+using Training20200822.Collections;
+using Training20200822.Extensions;
+using Training20200822.Numerics;
+using Training20200822.Questions;
+
+namespace Training20200822.Questions
+{
+    /// <summary>
+    /// https://atcoder.jp/contests/abc059/tasks/abc059_a
+    /// </summary>
+    public class QuestionA : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            var (s, t, u) = inputStream.ReadValue<string, string, string>();
+            yield return (s.Substring(0, 1) + t.Substring(0, 1) + u.Substring(0, 1)).ToUpper();
+        }
+    }
+}
---
+++ Training20200822/Training20200822/Training20200822/Questions/QuestionF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Training20200822.Algorithms;
+using Training20200822.Collections;
+using Training20200822.Extensions;
+using Training20200822.Numerics;
+using Training20200822.Questions;
+
+namespace Training20200822.Questions
+{
+    public class QuestionF : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

ce07aae4d666135bfab7446d5cb4614223335fdb
terry_u16 <terry-u16@users.noreply.github.com>
2020-08-30 01:39:50 +0900

    Add Training20200828

---
+++ Training20200828/Training20200828/Training20200828.Test/AtCoderTester.cs
@@ -0,0 +1,132 @@
+using System;
+using Xunit;
+using Training20200828.Questions;
+using System.Collections.Generic;
+using System.Linq;
+using Training20200828.Collections;
+
+namespace Training20200828.Test
+{
+    public class AtCoderTester
+    {
+        [Theory]
+        [InlineData(@"4 2 3
+2 3
+0 1
+1 2
+1 3", @"1")]
+        [InlineData(@"4 1 4
+3
+0 1
+0 2
+1 3
+2 3", @"1")]
+        [InlineData(@"10 3 11
+7 8 9
+0 1
+0 2
+0 3
+0 4
+1 5
+2 5
+5 6
+6 7
+6 8
+3 9
+4 9", @"2")]
+        [InlineData(@"6 2 6
+4 5
+0 1
+0 2
+1 3
+2 3
+3 4
+3 5", @"2")]
+        [InlineData(@"4 3 3
+1 2 3
+1 2
+1 3
+2 3", @"0")]
+        public void QuestionATest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionA();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        //[Theory]
+        //[InlineData(@"", @"")]
+        public void QuestionBTest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionB();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        //[Theory]
+        //[InlineData(@"", @"")]
+        public void QuestionCTest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionC();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        //[Theory]
+        //[InlineData(@"", @"")]
+        public void QuestionDTest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionD();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        //[Theory]
+        //[InlineData(@"", @"")]
+        public void QuestionETest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionE();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        //[Theory]
+        //[InlineData(@"", @"")]
+        public void QuestionFTest(string input, string output)
+        {
+            var outputs = SplitByNewLine(output);
+            IAtCoderQuestion question = new QuestionF();
+
+            var answers = question.Solve(input).Select(o => o.ToString()).ToArray();
+
+            Assert.Equal(outputs, answers);
+        }
+
+        void AssertNearlyEqual(IEnumerable<string> expected, IEnumerable<string> actual, double acceptableError = 1e-6)
+        {
+            Assert.Equal(expected.Count(), actual.Count());
+            foreach (var (exp, act) in (expected, actual).Zip().Select(p => (double.Parse(p.v1), double.Parse(p.v2))))
+            {
+                var error = act - exp;
+                Assert.InRange(Math.Abs(error), 0, acceptableError);
+            }
+        }
+
+        IEnumerable<string> SplitByNewLine(string input) => input?.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None) ?? new string[0];
+    }
+}
---
+++ Training20200828/Training20200828/Training20200828/Questions/QuestionA.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Training20200828.Algorithms;
+using Training20200828.Collections;
+using Training20200828.Extensions;
+using Training20200828.Numerics;
+using Training20200828.Questions;
+
+namespace Training20200828.Questions
+{
+    public class QuestionA : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            var (userCount, markedCount, friendshipsCount) = inputStream.ReadValue<int, int, int>();
+            var startNode = userCount;
+            var endNode = userCount + 1;
+            var graph = Enumerable.Repeat(0, userCount + 2).Select(_ => new List<Edge>()).ToArray();
+
+            AddDirectionalEdge(startNode, 0, 1 << 28);
+
+            if (markedCount > 0)
+            {
+                foreach (var marked in inputStream.ReadIntArray())
+                {
+                    AddDirectionalEdge(marked, endNode, 1);
+                }
+            }
+            else
+            {
+                _ = inputStream.ReadLine();
+            }
+
+            for (int i = 0; i < friendshipsCount; i++)
+            {
+                var (a, b) = inputStream.ReadValue<int, int>();
+                AddNonDirectionalEdge(a, b, 1);
+            }
+
+            var used = new bool[graph.Length];
+
+            var flow = 0;
+            while (true)
+            {
+                Array.Clear(used, 0, used.Length);
+                var f = Dfs(startNode, 1 << 28);
+                if (f == 0)
+                {
+                    break;
+                }
+                flow += f;
+            }
+
+            yield return flow;
+
+            int Dfs(int current, int flow)
+            {
+                if (current == endNode)
+                {
+                    return flow;
+                }
+                else
+                {
+                    used[current] = true;
+                    foreach (var edge in graph[current])
+                    {
+                        if (!used[edge.To] && edge.Capacity > 0)
+                        {
+                            var f = Dfs(edge.To, Math.Min(flow, edge.Capacity));
+                            if (f > 0)
+                            {
+                                edge.Capacity -= f;
+                                graph[edge.To][edge.Reverese].Capacity += f;
+                                return f;
+                            }
+                        }
+                    }
+
+                    return 0;
+                }
+            }
+
+            void AddDirectionalEdge(int from, int to, int capacity)
+            {
+                graph[from].Add(new Edge(to, capacity, graph[to].Count));
+                graph[to].Add(new Edge(from, 0, graph[from].Count - 1));
+            }
+
+            void AddNonDirectionalEdge(int from, int to, int capacity)
+            {
+                graph[from].Add(new Edge(to, capacity, graph[to].Count));
+                graph[to].Add(new Edge(from, capacity, graph[from].Count - 1));
+            }
+        }
+
+        class Edge
+        {
+            public int To { get; }
+            public int Capacity { get; set; }
+            public int Reverese { get; }
+
+            public Edge(int to, int capacity, int reverse)
+            {
+                To = to;
+                Capacity = capacity;
+                Reverese = reverse;
+            }
+
+            public override string ToString() => $"{nameof(To)}: {To}, {nameof(Capacity)}: {Capacity}";
+        }
+    }
+}

bcd8a554ee84e6f26f881c66b250d3077244cc2d
terry_u16 <terry-u16@users.noreply.github.com>
2020-09-04 00:27:51 +0900

    Add Kujikatsu078

---
+++ Kujikatsu078/Kujikatsu078/Kujikatsu078/Questions/QuestionE.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Kujikatsu078.Algorithms;
+using Kujikatsu078.Collections;
+using Kujikatsu078.Extensions;
+using Kujikatsu078.Numerics;
+using Kujikatsu078.Questions;
+
+namespace Kujikatsu078.Questions
+{
+    public class QuestionE : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

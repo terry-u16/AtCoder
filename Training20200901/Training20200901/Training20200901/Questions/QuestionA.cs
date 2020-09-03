d4a2a5f038d87b85baca8820bdf7a7ded3dbd27f
terry_u16 <terry-u16@users.noreply.github.com>
2020-09-04 00:27:23 +0900

    Add Training20200901

---
+++ Training20200901/Training20200901/Training20200901/Questions/QuestionA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Training20200901.Algorithms;
+using Training20200901.Collections;
+using Training20200901.Extensions;
+using Training20200901.Numerics;
+using Training20200901.Questions;
+
+namespace Training20200901.Questions
+{
+    /// <summary>
+    /// https://atcoder.jp/contests/abc113/tasks/abc113_a
+    /// </summary>
+    public class QuestionA : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            var (x, y) = inputStream.ReadValue<int, int>();
+            yield return x + y / 2;
+        }
+    }
+}
---
+++ Training20200901/Training20200901/Training20200901/Questions/QuestionC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Training20200901.Algorithms;
+using Training20200901.Collections;
+using Training20200901.Extensions;
+using Training20200901.Numerics;
+using Training20200901.Questions;
+
+namespace Training20200901.Questions
+{
+    public class QuestionC : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

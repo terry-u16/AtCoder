94ef668a297562c8581150850300f6411221044b
terry_u16 <terry-u16@users.noreply.github.com>
2020-09-13 15:00:26 +0900

    Add AtCoderLibraryPracticeContest

---
+++ AtCoderLibraryPracticeContest/AtCoderLibraryPracticeContest/AtCoderLibraryPracticeContest/Questions/QuestionF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using AtCoderLibraryPracticeContest.Algorithms;
+using AtCoderLibraryPracticeContest.Collections;
+using AtCoderLibraryPracticeContest.Extensions;
+using AtCoderLibraryPracticeContest.Numerics;
+using AtCoderLibraryPracticeContest.Questions;
+
+namespace AtCoderLibraryPracticeContest.Questions
+{
+    public class QuestionF : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

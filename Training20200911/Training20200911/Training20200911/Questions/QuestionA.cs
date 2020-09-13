9c04b580e23495b8226747d47401539ee8b27871
terry_u16 <terry-u16@users.noreply.github.com>
2020-09-13 15:01:20 +0900

    Add Training20200911

---
+++ Training20200911/Training20200911/Training20200911/Questions/QuestionA.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Training20200911.Algorithms;
+using Training20200911.Collections;
+using Training20200911.Extensions;
+using Training20200911.Numerics;
+using Training20200911.Questions;
+
+namespace Training20200911.Questions
+{
+    /// <summary>
+    /// https://atcoder.jp/contests/abc040/tasks/abc040_b
+    /// </summary>
+    public class QuestionA : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            var n = inputStream.ReadInt();
+            var min = int.MaxValue;
+
+            if (n == 1)
+            {
+                yield return 0;
+                yield break;
+            }
+
+            for (int row = 1; row < n; row++)
+            {
+                var column = n / row;
+                var mod = n % row;
+                min = Math.Min(min, Math.Abs(row - column) + mod);
+            }
+
+            yield return min;
+        }
+    }
+}
---
+++ Training20200911/Training20200911/Training20200911/Questions/QuestionB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Training20200911.Algorithms;
+using Training20200911.Collections;
+using Training20200911.Extensions;
+using Training20200911.Numerics;
+using Training20200911.Questions;
+
+namespace Training20200911.Questions
+{
+    public class QuestionB : AtCoderQuestionBase
+    {
+        public override IEnumerable<object> Solve(TextReader inputStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

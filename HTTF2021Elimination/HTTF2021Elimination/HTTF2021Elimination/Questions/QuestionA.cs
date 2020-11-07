30f2f3a2e06d8acd771b45153308abaf861069e4
terry_u16 <terry-u16@users.noreply.github.com>
2020-11-07 18:45:39 +0900

    Revert "Revert "Make diff faster (but buggy)""
    This reverts commit cd9efa30a034c3f6d97ea5bc346cb2fefd1fa5ba.

--- HTTF2021Elimination/HTTF2021Elimination/HTTF2021Elimination/Questions/QuestionA.cs
+++ HTTF2021Elimination/HTTF2021Elimination/HTTF2021Elimination/Questions/QuestionA.cs
@@ -40,8 +40,18 @@ namespace HTTF2021Elimination.Questions
     {
         const int Size = 20;
         const int TimeLimit = 2900;
+        const int SecondRow = 10;
+        const int SecondColumn = 0;
         readonly Coordinate[] _cards;
+
+        /// <summary>
+        /// <see cref="_takeOrder"/>[order] = card;
+        /// </summary>
         readonly int[] _takeOrder;
+
+        /// <summary>
+        /// <see cref="_takeOrderInv"/>[card] = order;
+        /// </summary>
         readonly int[] _takeOrderInv;
         readonly Coordinate[] _compressed;
 
@@ -74,9 +84,9 @@ namespace HTTF2021Elimination.Questions
                 AppendMove(9 - row, 0);
             }
 
-            if (column > 9)
+            if (column > 10)
             {
-                AppendMove(0, 9 - column);
+                AppendMove(0, 10 - column);
             }
 
             for (int r = 0; r < 10; r++)
@@ -103,8 +113,8 @@ namespace HTTF2021Elimination.Questions
                 }
             }
 
-            row = 10;
-            column = 0;
+            row = SecondRow;
+            column = SecondColumn;
 
             foreach (var (r, c) in _compressed)
             {
@@ -138,13 +148,13 @@ namespace HTTF2021Elimination.Questions
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
         public void Annealing(Stopwatch sw)
         {
             var random = new XorShift();
-            var prev = CalculateCost();
             var count = 0;
             var startTime = sw.ElapsedMilliseconds;
-            var temp = CalculateTemp(startTime, startTime, TimeLimit);
+            var temperature = CalculateTemp(startTime, startTime, TimeLimit);
 
             while (sw.ElapsedMilliseconds < TimeLimit)
             {
@@ -153,27 +163,13 @@ namespace HTTF2021Elimination.Questions
 
                 if (count++ % 100 == 0)
                 {
-                    temp = CalculateTemp(sw.ElapsedMilliseconds, startTime, TimeLimit);
+                    temperature = CalculateTemp(sw.ElapsedMilliseconds, startTime, TimeLimit);
                 }
 
-                if (cardA == cardB)
-                {
-                    continue;
-                }
+                // 大きい方が優秀
+                var diff = -CalculateDiff(cardA, cardB);
 
-                Swap(ref _takeOrderInv[cardA], ref _takeOrderInv[cardB]);
-                Swap(ref _takeOrder[_takeOrderInv[cardA]], ref _takeOrder[_takeOrderInv[cardB]]);
-                Swap(ref _compressed[cardA], ref _compressed[cardB]);
-                var next = CalculateCost();
-
-                // 小さい方が優秀
-                var diff = prev - next;
-
-                if (diff >= 0 || random.NextDouble() <= Math.Exp(diff / temp))
-                {
-                    prev = next;
-                }
-                else
+                if (diff >= 0 || random.NextDouble() <= Math.Exp(diff / temperature))
                 {
                     Swap(ref _takeOrderInv[cardA], ref _takeOrderInv[cardB]);
                     Swap(ref _takeOrder[_takeOrderInv[cardA]], ref _takeOrder[_takeOrderInv[cardB]]);
@@ -182,9 +178,181 @@ namespace HTTF2021Elimination.Questions
             }
         }
 
+        int CalculateDiff(int cardNoA, int cardNoB)
+        {
+            if (cardNoA == cardNoB)
+            {
+                return 0;
+            }
+
+            var orderA = _takeOrderInv[cardNoA];
+            var orderB = _takeOrderInv[cardNoB];
+
+            // 小さい方が優秀
+            var diff = 0;
+
+            // A -> Bの順番で取るようにしておく
+            if (orderA.SwapIfLargerThan(ref orderB))
+            {
+                Swap(ref cardNoA, ref cardNoB);
+            }
+
+            var cardA = _cards[cardNoA];
+            var cardB = _cards[cardNoB];
+            var compA = _compressed[cardNoA];
+            var compB = _compressed[cardNoB];
+
+            // 最初の収集
+            if (orderB - orderA == 1)
+            {
+                var last = new Coordinate();
+                var prevOrder = orderA - 1;
+
+                if (unchecked((uint)prevOrder < (uint)_takeOrder.Length))
+                {
+                    last = _cards[_takeOrder[prevOrder]];
+                }
+
+                var prevCost = 0;
+                prevCost += last.GetDistanceTo(cardA);
+                prevCost += cardA.GetDistanceTo(cardB);
+
+                var nextCost = 0;
+                nextCost += last.GetDistanceTo(cardB);
+                nextCost += cardB.GetDistanceTo(cardA);
+
+                var nextOrder = orderB + 1;
+                if (unchecked((uint)nextOrder < (uint)_takeOrder.Length))
+                {
+                    var nextCard = _cards[_takeOrder[nextOrder]];
+                    prevCost += cardB.GetDistanceTo(nextCard);
+                    nextCost += cardA.GetDistanceTo(nextCard);
+                }
+                diff += nextCost - prevCost;
+            }
+            else
+            {
+                var prevCost = GetCollectCost(cardA, orderA) + GetCollectCost(cardB, orderB);
+                var nextCost = GetCollectCost(cardB, orderA) + GetCollectCost(cardA, orderB);
+                diff += nextCost - prevCost;
+            }
+
+            // 最後の並べ替え
+            if (orderB - orderA == 1)
+            {
+                var last = new Coordinate(SecondRow, SecondColumn);
+                var prevOrder = orderA - 1;
+
+                if (unchecked((uint)prevOrder < (uint)_takeOrder.Length))
+                {
+                    last = _compressed[_takeOrder[prevOrder]];
+                }
+
+                var prevCost = 0;
+                prevCost += last.GetDistanceTo(compA);
+                prevCost += compA.GetDistanceTo(compB);
+
+                var nextCost = 0;
+                nextCost += last.GetDistanceTo(compB);
+                nextCost += compB.GetDistanceTo(compA);
+
+                var nextOrder = orderB + 1;
+                if (unchecked((uint)nextOrder < (uint)_takeOrder.Length))
+                {
+                    var nextCard = _compressed[_takeOrder[nextOrder]];
+                    prevCost += compB.GetDistanceTo(nextCard);
+                    nextCost += compA.GetDistanceTo(nextCard);
+                }
+
+                diff += nextCost - prevCost;
+            }
+            else
+            {
+                var prevCost = GetOrderCost(compA, orderA) + GetOrderCost(compB, orderB);
+                var nextCost = GetOrderCost(compB, orderA) + GetOrderCost(compA, orderB);
+                diff += nextCost - prevCost;
+            }
+
+            return diff;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        int GetCollectCost(Coordinate card, int order)
+        {
+            var last = new Coordinate();
+            var prevOrder = order - 1;
+
+            if (unchecked((uint)prevOrder < (uint)_takeOrder.Length))
+            {
+                last = _cards[_takeOrder[prevOrder]];
+            }
+
+            var cost = 0;
+            cost += last.GetDistanceTo(card);
+
+            var nextOrder = order + 1;
+            if (unchecked((uint)nextOrder < (uint)_takeOrder.Length))
+            {
+                var nextCard = _cards[_takeOrder[nextOrder]];
+                cost += card.GetDistanceTo(nextCard);
+            }
+
+            return cost;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        int GetOrderCost(Coordinate card, int order)
+        {
+            var last = new Coordinate(SecondRow, SecondColumn);
+            var prevOrder = order - 1;
+
+            if (unchecked((uint)prevOrder < (uint)_takeOrder.Length))
+            {
+                last = _compressed[_takeOrder[prevOrder]];
+            }
+
+            var cost = 0;
+            cost += last.GetDistanceTo(card);
+
+            var nextOrder = order + 1;
+            if (unchecked((uint)nextOrder < (uint)_takeOrder.Length))
+            {
+                var nextCard = _compressed[_takeOrder[nextOrder]];
+                cost += card.GetDistanceTo(nextCard);
+            }
+
+            return cost;
+        }
+
+        int CalculateCost()
+        {
+            var cost = 0;
+            var row = 0;
+            var column = 0;
+
+            foreach (var i in _takeOrder)
+            {
+                var (r, c) = _cards[i];
+                cost += GetDistance(row - r, column - c);
+                row = r;
+                column = c;
+            }
+
+            (row, column) = _compressed[_takeOrder[0]];
+
+            foreach (var (r, c) in _compressed)
+            {
+                cost += GetDistance(row - r, column - c);
+                row = r;
+                column = c;
+            }
+
+            return cost;
+        }
+
         double CalculateTemp(long currentTime, long startTime, long endTime)
         {
-            const double startTemp = 10;
+            const double startTemp = 20;
             const double endTemp = 0;
             var duration = endTime - startTime;
             var dt = currentTime - startTime;
@@ -259,31 +427,6 @@ namespace HTTF2021Elimination.Questions
             }
         }
 
-        int CalculateCost()
-        {
-            var cost = 0;
-            var row = 0;
-            var column = 0;
-
-            foreach (var i in _takeOrder)
-            {
-                var (r, c) = _cards[i];
-                cost += GetDistance(row - r, column - c);
-                row = r;
-                column = c;
-            }
-
-            (row, column) = _compressed[_takeOrder[0]];
-
-            foreach (var (r, c) in _compressed)
-            {
-                cost += GetDistance(row - r, column - c);
-                row = r;
-                column = c;
-            }
-
-            return cost;
-        }
 
         int GetDistance(int dr, int dc) => Math.Abs(dr) + Math.Abs(dc);
 
@@ -322,6 +465,9 @@ namespace HTTF2021Elimination.Questions
             Row = row;
             Column = column;
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int GetDistanceTo(Coordinate other) => Math.Abs(Row - other.Row) + Math.Abs(Column - other.Column);
 
         public void Deconstruct(out int row, out int column) => (row, column) = (Row, Column);
         public override string ToString() => $"{nameof(Row)}: {Row}, {nameof(Column)}: {Column}";

8176184264a4e61de2697a68f9edd31175765e55
terry_u16 <terry-u16@users.noreply.github.com>
2020-11-07 18:44:08 +0900

    Revert "Revert "Add annealing""
    This reverts commit 2551eb638fd33530a67d772c08d9eee9610478c1.

--- HTTF2021Elimination/HTTF2021Elimination/HTTF2021Elimination/Questions/QuestionA.cs
+++ HTTF2021Elimination/HTTF2021Elimination/HTTF2021Elimination/Questions/QuestionA.cs
@@ -20,6 +20,9 @@ namespace HTTF2021Elimination.Questions
     {
         public override void Solve(IOManager io)
         {
+            var sw = new Stopwatch();
+            sw.Start();
+
             var cards = new Coordinate[100];
             for (int i = 0; i < cards.Length; i++)
             {
@@ -27,139 +30,88 @@ namespace HTTF2021Elimination.Questions
             }
 
             var solver = new Solver(cards);
-            var firstResult = solver.Solve();
+            solver.Annealing(sw);
 
-            for (int i = 0; i < cards.Length; i++)
-            {
-                var (r, c) = cards[i];
-                cards[i] = new Coordinate(c, r);
-            }
-
-            solver = new Solver(cards);
-            var secondResult = solver.Solve();
-
-            var firstScore = firstResult.Count(c => c == 'U' || c == 'D' || c == 'L' || c == 'R');
-            var secondScore = secondResult.Count(c => c == 'U' || c == 'D' || c == 'L' || c == 'R');
-
-            if (firstScore < secondScore)
-            {
-                io.WriteLine(firstResult);
-            }
-            else
-            {
-                var builder = new StringBuilder();
-
-                foreach (var c in secondResult)
-                {
-                    builder.Append(c switch
-                    {
-                        'U' => 'L',
-                        'L' => 'U',
-                        'R' => 'D',
-                        'D' => 'R',
-                        _ => c
-                    });
-                }
-
-                io.WriteLine(builder.ToString());
-            }
+            io.WriteLine(solver.GetResult());
         }
     }
 
     public class Solver
     {
         const int Size = 20;
+        const int TimeLimit = 2900;
         readonly Coordinate[] _cards;
+        readonly int[] _takeOrder;
+        readonly int[] _takeOrderInv;
+        readonly Coordinate[] _compressed;
 
         public Solver(Coordinate[] cards)
         {
             _cards = cards;
+            _takeOrder = new int[cards.Length];
+            _takeOrderInv = new int[cards.Length];
+            _compressed = new Coordinate[cards.Length];
+            Initialize();
         }
 
-        public string Solve()
+        public string GetResult()
         {
-            var map = new int[Size, Size];
-            map.Fill(-1);
-            for (int i = 0; i < _cards.Length; i++)
+            var result = new StringBuilder();
+            var row = 0;
+            var column = 0;
+
+            foreach (var i in _takeOrder)
             {
                 var (r, c) = _cards[i];
-                map[r, c] = i;
-            }
-
-            var takahashiRow = 0;
-            var takahashiColumn = 0;
-            var result = new StringBuilder();
-            var stack = new Stack<int>();
-
-            for (int row = 0; row < Size; row++)
-            {
-                if (row % 2 == 0)
-                {
-                    for (int column = 0; column < Size; column++)
-                    {
-                        CheckAndAdd(row, column);
-                    }
-                }
-                else
-                {
-                    for (int column = Size - 1; column >= 0; column--)
-                    {
-                        CheckAndAdd(row, column);
-                    }
-                }
-
-                void CheckAndAdd(int row, int column)
-                {
-                    if (map[row, column] != -1)
-                    {
-                        stack.Push(map[row, column]);
-                        AppendMove(row - takahashiRow, column - takahashiColumn);
-                        result.Append('I');
-                        takahashiRow = row;
-                        takahashiColumn = column;
-                    }
-                }
-            }
-
-            AppendMove(Size - 1 - takahashiRow, 0 - takahashiColumn);
-            takahashiRow = Size - 1;
-            takahashiColumn = 0;
-
-            var currentCards = new Coordinate[_cards.Length];
-
-            for (int row = Size - 1; row >= 10; row--)
-            {
-                if (row % 2 == 1)
-                {
-                    for (int column = 0; column < 10; column++)
-                    {
-                        Drop(row, column);
-                    }
-                }
-                else
-                {
-                    for (int column = 10 - 1; column >= 0; column--)
-                    {
-                        Drop(row, column);
-                    }
-                }
-
-                void Drop(int row, int column)
-                {
-                    currentCards[stack.Pop()] = new Coordinate(row, column);
-                    AppendMove(row - takahashiRow, column - takahashiColumn);
-                    result.Append('O');
-                    takahashiRow = row;
-                    takahashiColumn = column;
-                }
-            }
-
-            foreach (var (row, column) in currentCards)
-            {
-                AppendMove(row - takahashiRow, column - takahashiColumn);
+                AppendMove(r - row, c - column);
                 result.Append('I');
-                takahashiRow = row;
-                takahashiColumn = column;
+                row = r;
+                column = c;
+            }
+
+            if (row < 9)
+            {
+                AppendMove(9 - row, 0);
+            }
+
+            if (column > 9)
+            {
+                AppendMove(0, 9 - column);
+            }
+
+            for (int r = 0; r < 10; r++)
+            {
+                for (int c = 0; c < 10; c++)
+                {
+                    result.Append('O');
+
+                    if (c < 9)
+                    {
+                        if (r % 2 == 0)
+                        {
+                            result.Append('R');
+                        }
+                        else
+                        {
+                            result.Append('L');
+                        }
+                    }
+                    else if (r < 9)
+                    {
+                        result.Append('U');
+                    }
+                }
+            }
+
+            row = 10;
+            column = 0;
+
+            foreach (var (r, c) in _compressed)
+            {
+                AppendMove(r - row, c - column);
+                result.Append('I');
+                row = r;
+                column = c;
             }
 
             return result.ToString();
@@ -184,7 +136,162 @@ namespace HTTF2021Elimination.Questions
                     result.Append('L', -dc);
                 }
             }
+        }
 
+        public void Annealing(Stopwatch sw)
+        {
+            var random = new XorShift();
+            var prev = CalculateCost();
+            var count = 0;
+            var startTime = sw.ElapsedMilliseconds;
+            var temp = CalculateTemp(startTime, startTime, TimeLimit);
+
+            while (sw.ElapsedMilliseconds < TimeLimit)
+            {
+                var cardA = random.Next(_takeOrder.Length);
+                var cardB = random.Next(_takeOrder.Length);
+
+                if (count++ % 100 == 0)
+                {
+                    temp = CalculateTemp(sw.ElapsedMilliseconds, startTime, TimeLimit);
+                }
+
+                if (cardA == cardB)
+                {
+                    continue;
+                }
+
+                Swap(ref _takeOrderInv[cardA], ref _takeOrderInv[cardB]);
+                Swap(ref _takeOrder[_takeOrderInv[cardA]], ref _takeOrder[_takeOrderInv[cardB]]);
+                Swap(ref _compressed[cardA], ref _compressed[cardB]);
+                var next = CalculateCost();
+
+                // 小さい方が優秀
+                var diff = prev - next;
+
+                if (diff >= 0 || random.NextDouble() <= Math.Exp(diff / temp))
+                {
+                    prev = next;
+                }
+                else
+                {
+                    Swap(ref _takeOrderInv[cardA], ref _takeOrderInv[cardB]);
+                    Swap(ref _takeOrder[_takeOrderInv[cardA]], ref _takeOrder[_takeOrderInv[cardB]]);
+                    Swap(ref _compressed[cardA], ref _compressed[cardB]);
+                }
+            }
+        }
+
+        double CalculateTemp(long currentTime, long startTime, long endTime)
+        {
+            const double startTemp = 10;
+            const double endTemp = 0;
+            var duration = endTime - startTime;
+            var dt = currentTime - startTime;
+            var temp = startTemp + (endTemp - startTemp) * dt / duration;
+            return temp;
+        }
+
+        void Initialize()
+        {
+            var map = new int[Size, Size];
+            map.Fill(-1);
+            for (int i = 0; i < _cards.Length; i++)
+            {
+                var (r, c) = _cards[i];
+                map[r, c] = i;
+            }
+
+            var stack = new Stack<int>();
+
+            var takenIndex = 0;
+
+            for (int row = 0; row < Size; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int column = 0; column < Size; column++)
+                    {
+                        CheckAndAdd(row, column);
+                    }
+                }
+                else
+                {
+                    for (int column = Size - 1; column >= 0; column--)
+                    {
+                        CheckAndAdd(row, column);
+                    }
+                }
+
+                void CheckAndAdd(int row, int column)
+                {
+                    if (map[row, column] != -1)
+                    {
+                        _takeOrder[takenIndex] = map[row, column];
+                        _takeOrderInv[map[row, column]] = takenIndex++;
+                        stack.Push(map[row, column]);
+                    }
+                }
+            }
+
+            for (int row = Size - 1; row >= 10; row--)
+            {
+                if (row % 2 == 1)
+                {
+                    for (int column = 0; column < 10; column++)
+                    {
+                        Drop(row, column);
+                    }
+                }
+                else
+                {
+                    for (int column = 10 - 1; column >= 0; column--)
+                    {
+                        Drop(row, column);
+                    }
+                }
+
+                void Drop(int row, int column)
+                {
+                    var card = stack.Pop();
+                    _compressed[card] = new Coordinate(row, column);
+                }
+            }
+        }
+
+        int CalculateCost()
+        {
+            var cost = 0;
+            var row = 0;
+            var column = 0;
+
+            foreach (var i in _takeOrder)
+            {
+                var (r, c) = _cards[i];
+                cost += GetDistance(row - r, column - c);
+                row = r;
+                column = c;
+            }
+
+            (row, column) = _compressed[_takeOrder[0]];
+
+            foreach (var (r, c) in _compressed)
+            {
+                cost += GetDistance(row - r, column - c);
+                row = r;
+                column = c;
+            }
+
+            return cost;
+        }
+
+        int GetDistance(int dr, int dc) => Math.Abs(dr) + Math.Abs(dc);
+
+        void Swap<T>(ref T a, ref T b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
         }
     }
 

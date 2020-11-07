9e70a242b9ef6de1fec593333976f35563c97335
terry_u16 <terry-u16@users.noreply.github.com>
2020-11-07 20:16:11 +0900

    Revert "Add multi-test"
    This reverts commit 0b066306f0fac51e8f2915d41b5492f56df558ce.

--- HTTF2021Elimination/HTTF2021Elimination/HTTF2021Elimination/Questions/QuestionA.cs
+++ HTTF2021Elimination/HTTF2021Elimination/HTTF2021Elimination/Questions/QuestionA.cs
@@ -29,24 +29,10 @@ namespace HTTF2021Elimination.Questions
                 cards[i] = new Coordinate(io.ReadInt(), io.ReadInt());
             }
 
-            var lastCost = int.MaxValue;
-            string result = "";
+            var solver = new Solver(cards);
+            solver.Annealing(sw);
 
-            for (int i = 1; i <= 5; i++)
-            {
-                var solver = new Solver(cards);
-                solver.Annealing(sw, 600 * i - 100);
-                var res = solver.GetResult();
-
-                var cost = res.Count(c => c == 'R' || c == 'D' || c == 'L' || c == 'U');
-
-                if (lastCost.ChangeMin(cost))
-                {
-                    result = res;
-                }
-            }
-
-            io.WriteLine(result);
+            io.WriteLine(solver.GetResult());
         }
     }
 
@@ -56,6 +42,7 @@ namespace HTTF2021Elimination.Questions
         const int TimeLimit = 2900;
         const int SecondRow = 10;
         const int SecondColumn = 0;
+        const double ProbabilityTwo = 0.8;
         readonly Coordinate[] _cards;
 
         /// <summary>
@@ -163,49 +150,55 @@ namespace HTTF2021Elimination.Questions
         }
 
         [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-        public void Annealing(Stopwatch sw, int timeLimit)
+        public void Annealing(Stopwatch sw)
         {
             var random = new XorShift();
             var count = 0;
             var startTime = sw.ElapsedMilliseconds;
-            var temperature = CalculateTemp(startTime, startTime, timeLimit);
+            var temperature = CalculateTemp(startTime, startTime, TimeLimit);
 
             while (true)
             {
                 if (count++ % 10000 == 0)
                 {
-                    var t = sw.ElapsedMilliseconds;
-                    temperature = CalculateTemp(t, startTime, timeLimit);
+                    temperature = CalculateTemp(sw.ElapsedMilliseconds, startTime, TimeLimit);
 
-                    if (t >= timeLimit)
+                    if (sw.ElapsedMilliseconds >= TimeLimit)
                     {
                         break;
                     }
                 }
 
-                var cards = new int[] { random.Next(_takeOrder.Length), random.Next(_takeOrder.Length) };
-
-                if (cards[0] == cards[1])
+                if (random.NextDouble() < ProbabilityTwo)
                 {
-                    continue;
-                }
+                    var cards = new int[] { random.Next(_takeOrder.Length), random.Next(_takeOrder.Length) };
 
-                var prev = CalculateLocal(cards);
+                    if (cards[0] == cards[1])
+                    {
+                        continue;
+                    }
 
-                Swap(ref _takeOrderInv[cards[0]], ref _takeOrderInv[cards[1]]);
-                Swap(ref _takeOrder[_takeOrderInv[cards[0]]], ref _takeOrder[_takeOrderInv[cards[1]]]);
-                Swap(ref _compressed[cards[0]], ref _compressed[cards[1]]);
+                    var prev = CalculateLocal(cards);
 
-                var next = CalculateLocal(cards);
-
-                // 大きい方が優秀
-                var diff = prev - next;
-
-                if (!(diff >= 0 || random.NextDouble() <= Math.Exp(diff / temperature)))
-                {
                     Swap(ref _takeOrderInv[cards[0]], ref _takeOrderInv[cards[1]]);
                     Swap(ref _takeOrder[_takeOrderInv[cards[0]]], ref _takeOrder[_takeOrderInv[cards[1]]]);
                     Swap(ref _compressed[cards[0]], ref _compressed[cards[1]]);
+
+                    var next = CalculateLocal(cards);
+
+                    // 大きい方が優秀
+                    var diff = prev - next;
+
+                    if (diff >= 0 || random.NextDouble() <= Math.Exp(diff / temperature))
+                    {
+                    }
+                    else
+                    {
+                        Swap(ref _takeOrderInv[cards[0]], ref _takeOrderInv[cards[1]]);
+                        Swap(ref _takeOrder[_takeOrderInv[cards[0]]], ref _takeOrder[_takeOrderInv[cards[1]]]);
+                        Swap(ref _compressed[cards[0]], ref _compressed[cards[1]]);
+                    }
+
                 }
             }
         }
